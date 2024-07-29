5572f891c5a3392366b2e1d0a7e11e9f6ac646a5
Andrew G <andrew.giannopoulos@gmail.com>
2024-07-29 21:05:01 +1000

    Added landing page, updated deployment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -9,6 +10,23 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/test")
+def read_root():
+    return {"TEST": "THING"}
+
+@app.get("/landing")
+def read_landing():
+    html_content = """
+    <html>
+        <head>
+            <title>Landing Page</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
