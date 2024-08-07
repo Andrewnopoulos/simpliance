c95708c2f7cb804fd51151dc4d75567ea98d6dca
Andrew G <andrew.giannopoulos@gmail.com>
2024-08-07 21:24:53 +1000

    authkey deletion

--- app/routes/authkeys.py
+++ app/routes/authkeys.py
@@ -17,3 +17,12 @@ def create_authkeys(user_id: str, keys: AuthKeys):
     with Storage() as s:
         s.insert(keys)
     return keys.dict()
+
+@authkeys_router.delete("/{user_id}/{role_id}")
+def delete_authkeys(user_id: str, role_id: str):
+    with Storage() as s:
+        authkeys = s.get_one(AuthKeys, {'role_id': role_id})
+        if not authkeys:
+            return {"error": "authkeys not found"}
+        success = s.delete(authkeys) == 1
+        return {"retval": "success" if success else "failed"}
