9d72698fc639d1e772ae31b0a7f51b00ec799fc6
Andrew G <andrew.giannopoulos@gmail.com>
2024-08-13 21:29:46 +1000

    Added basic secure functions

---
+++ app/routes/security.py
@@ -0,0 +1,127 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import uuid
+
+import jwt
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from data.datastore import Storage
+from data.models import User, AuthKeys, Report
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "f1592cad353ec77355dd864e1499700b97b7b3285a7a43cd954dccbeb38ec1ae"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="security/token")
+
+secure_router = APIRouter(prefix='/security',
+                          tags=['security'])
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def authenticate_user(email: str, password: str):
+    with Storage() as s:
+        user = s.get_one(User, {"email": email})
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except InvalidTokenError:
+        raise credentials_exception
+    with Storage() as s:
+        user = s.get_one(User, {"email": token_data.email})
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@secure_router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+@secure_router.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+@secure_router.post("/users/register")
+async def register_user(name: str, email: str, password: str):
+    u = User(str(uuid.uuid4()), name, email)
+    u.hashed_password = get_password_hash(password)
+    with Storage() as s:
+        s.insert(u)
+
+    return u
+
+@secure_router.get("/users/me/reports/")
+async def read_own_reports(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    with Storage() as s:
+        user_reports = s.get_where(Report, {'user_id': current_user.id})
+    return [{"reports": user_reports, "owner": current_user.name}]
