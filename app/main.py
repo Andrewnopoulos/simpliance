c3c1afb878fef6000852ac254f1eee13d7f38ec1
Andrew <andrew.giannopoulos@gmail.com>
2024-10-19 09:21:41 +1100

    allow all origins for backend comms

--- app/main.py
+++ app/main.py
@@ -37,16 +37,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "http://localhost:5173",
-                   "http://localhost:4173",
-                   "http://127.0.0.1:3000",
-                   "http://127.0.0.1:5173",
-                   "http://127.0.0.1:4173",
-                   "http://frontend:3000",
-                   f"http://{BALANCER}",
-                   f"http://{DEPLOY_URL}"
-                   ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
